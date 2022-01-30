3b994eac50d097fa8773f8efe84ad84b754b703b
AlejandroVazquez019 <91817641+AlejandroVazquez019@users.noreply.github.com>
2022-01-30 17:53:28 -0600

    Add files via upload

---
+++ Peliculas/Peliculas/Controllers/ValuesController.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Peliculas.Models.Response;
+using Peliculas.Models;
+using Peliculas.Models.Request;
+
+namespace Peliculas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            Respuesta oRespuesta = new Respuesta();
+
+            try
+            {
+                using (PeliculasContext db = new PeliculasContext())
+                {
+                    var lst = db.Pelis.ToList();
+                    oRespuesta.Exito = 1;
+                    oRespuesta.Data = lst;
+                }
+            }
+            catch (Exception ex)
+            {
+                oRespuesta.Mensaje = ex.Message;
+
+            }
+            return Ok(oRespuesta);
+        }
+
+        [HttpPost]
+        public IActionResult Add(PeliculasRequest Models)
+        {
+            Respuesta oRespuesta = new Respuesta();
+            try
+            {
+                using (PeliculasContext db = new PeliculasContext())
+                {
+                    Peli Opeli = new Peli();
+                    Opeli.Id = Models.Id;
+                    Opeli.Titulo = Models.Titulo;
+                    Opeli.Director = Models.Director;
+                    Opeli.Genero = Models.Genero;
+                    Opeli.Puntuacion = Models.Puntuacion;
+                    Opeli.Rating = Models.Rating;
+                    Opeli.FechaPublicacion = Models.FechaPublicacion;
+                    db.Pelis.Add(Opeli);
+                    db.SaveChanges();
+                    oRespuesta.Exito = 1;
+                }
+            }
+            catch (Exception ex)
+            {
+                oRespuesta.Mensaje = ex.Message;
+            }
+            return Ok(oRespuesta);
+        }
+
+        //METODO PARA EDITAR
+        [HttpPut]
+        public IActionResult Editar(PeliculasRequest Models)
+        {
+            Respuesta oRespuesta = new Respuesta();
+            try
+            {
+                using (PeliculasContext db = new PeliculasContext())
+                {
+
+                    //ID para modificar los datos
+                    Peli Opeli = db.Pelis.Find(Models.Id);
+                    Opeli.Titulo = Models.Titulo;
+                    Opeli.Director = Models.Director;
+                    Opeli.Genero = Models.Genero;
+                    Opeli.Puntuacion = Models.Puntuacion;
+                    Opeli.Rating = Models.Rating;
+                    Opeli.FechaPublicacion = Models.FechaPublicacion;
+                    //Indica que se modifico
+                    db.Entry(Opeli).State = Microsoft.EntityFrameworkCore.EntityState.Modified; ;
+                    db.SaveChanges();
+                    oRespuesta.Exito = 1;
+                }
+            }
+            catch (Exception ex)
+            {
+                oRespuesta.Mensaje = ex.Message;
+            }
+            return Ok(oRespuesta);
+        }
+
+        //METODO PARA ELIMINAR EL ID
+        [HttpDelete("{id}")]
+        public IActionResult Eliminar(int Id)
+        {
+            Respuesta oRespuesta = new Respuesta();
+            try
+            {
+                using (PeliculasContext db = new PeliculasContext())
+                {
+
+                    //para eliminar una pelicula con el ID
+                    Peli Opeli = db.Pelis.Find(Id);
+
+                    //
+                    //elimina los datos en el Registro
+                    db.Remove(Opeli);
+                    db.SaveChanges();
+                    oRespuesta.Exito = 1;
+                }
+            }
+            catch (Exception ex)
+            {
+                oRespuesta.Mensaje = ex.Message;
+            }
+            return Ok(oRespuesta);
+        }
+    }
+}
---
+++ Peliculas/Peliculas/Models/PeliculasContext.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace Peliculas.Models
+{
+    public partial class PeliculasContext : DbContext
+    {
+        public PeliculasContext()
+        {
+        }
+
+        public PeliculasContext(DbContextOptions<PeliculasContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Peli> Pelis { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=DESKTOP019\\SQLEXPRESS;user=Alex;password=12345;database=Peliculas;Persist Security info=false");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Peli>(entity =>
+            {
+                entity.Property(e => e.Director)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.FechaPublicacion)
+                    .HasColumnType("datetime")
+                    .HasColumnName("Fecha_Publicacion");
+
+                entity.Property(e => e.Genero)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Puntuacion)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Rating)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Titulo)
+                    .IsRequired()
+                    .HasMaxLength(50);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
